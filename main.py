ec2470a39802f211246261267f1ed28f9bfc21e2
RalleyD <0ralld67@solent.ac.uk>
2024-12-30 19:59:18 +0000

    ensure model dir is present before training

--- main.py
+++ main.py
@@ -8,6 +8,10 @@
     Runs the flask app where the webapp can be reached
 """
 from app import app
+import os.path
 
 if __name__ == "__main__":
+    if not os.path.exists('models/chatbot_model.keras'):
+        raise RuntimeError(
+            "Train the model before starting the application: model not found")
     app.run()
